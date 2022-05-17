8d7cd482480ad99db0e4f11a587952c835881b00
Anthony <parckeranthony@gmail.com>
2022-05-17 15:55:20 +0100

    Cleared unnecessary unused object created at run-time to reduce the cost around  garbage collection

--- Pokemon.Model/PokeApiModels/PokemonSpeciesBaseResponseObject.cs
+++ Pokemon.Model/PokeApiModels/PokemonSpeciesBaseResponseObject.cs
@@ -6,47 +6,7 @@ using System.Text.Json.Serialization;
 namespace Pokemon.Model.PokemonSpecies
 {
    
-    public class Area
-    {
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("url")]
-        public string Url { get; set; }
-    }
-
-    public class Color
-    {
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("url")]
-        public string Url { get; set; }
-    }
-
-    public class EggGroup
-    {
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("url")]
-        public string Url { get; set; }
-    }
-
-    public class EvolutionChain
-    {
-        [JsonPropertyName("url")]
-        public string Url { get; set; }
-    }
-
-    public class EvolvesFromSpecies
-    {
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("url")]
-        public string Url { get; set; }
-    }
+    
 
     public class FlavorTextEntry
     {
@@ -60,41 +20,7 @@ namespace Pokemon.Model.PokemonSpecies
         public Version Version { get; set; }
     }
 
-    public class Genera
-    {
-        [JsonPropertyName("genus")]
-        public string Genus { get; set; }
-
-        [JsonPropertyName("language")]
-        public Language Language { get; set; }
-    }
-
-    public class Generation
-    {
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("url")]
-        public string Url { get; set; }
-    }
-
-    public class GrowthRate
-    {
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("url")]
-        public string Url { get; set; }
-    }
-
-    public class Habitat
-    {
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("url")]
-        public string Url { get; set; }
-    }
+   
 
     public class Language
     {
@@ -105,155 +31,19 @@ namespace Pokemon.Model.PokemonSpecies
         public string Url { get; set; }
     }
 
-    public class Name
-    {
-        [JsonPropertyName("language")]
-        public Language Language { get; set; }
-
-        [JsonPropertyName("name")]
-        public string Name_ { get; set; }
-    }
-
-    public class PalParkEncounter
-    {
-        [JsonPropertyName("area")]
-        public Area Area { get; set; }
-
-        [JsonPropertyName("base_score")]
-        public int BaseScore { get; set; }
-
-        [JsonPropertyName("rate")]
-        public int Rate { get; set; }
-    }
-
-    public class Pokedex
-    {
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("url")]
-        public string Url { get; set; }
-    }
-
-    public class PokedexNumber
-    {
-        [JsonPropertyName("entry_number")]
-        public int EntryNumber { get; set; }
-
-        [JsonPropertyName("pokedex")]
-        public Pokedex Pokedex { get; set; }
-    }
-
-    public class Pokemon
-    {
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("url")]
-        public string Url { get; set; }
-    }
+   
 
     public class PokemonSpeciesBaseResponseObject
     {
-        [JsonPropertyName("base_happiness")]
-        public int BaseHappiness { get; set; }
-
-        [JsonPropertyName("capture_rate")]
-        public int CaptureRate { get; set; }
-
-        [JsonPropertyName("color")]
-        public Color Color { get; set; }
-
-        [JsonPropertyName("egg_groups")]
-        public IEnumerable<EggGroup> EggGroups { get; set; }
-
-        [JsonPropertyName("evolution_chain")]
-        public EvolutionChain EvolutionChain { get; set; }
-
-        [JsonPropertyName("evolves_from_species")]
-        public EvolvesFromSpecies EvolvesFromSpecies { get; set; }
+       
 
         [JsonPropertyName("flavor_text_entries")]
         public IEnumerable<FlavorTextEntry> FlavorTextEntries { get; set; }
 
-        [JsonPropertyName("form_descriptions")]
-        public IEnumerable<object> FormDescriptions { get; set; }
-
-        [JsonPropertyName("forms_switchable")]
-        public bool FormsSwitchable { get; set; }
-
-        [JsonPropertyName("gender_rate")]
-        public int GenderRate { get; set; }
-
-        [JsonPropertyName("genera")]
-        public IEnumerable<Genera> Genera { get; set; }
-
-        [JsonPropertyName("generation")]
-        public Generation Generation { get; set; }
-
-        [JsonPropertyName("growth_rate")]
-        public GrowthRate GrowthRate { get; set; }
-
-        [JsonPropertyName("habitat")]
-        public Habitat Habitat { get; set; }
-
-        [JsonPropertyName("has_gender_differences")]
-        public bool HasGenderDifferences { get; set; }
-
-        [JsonPropertyName("hatch_counter")]
-        public int HatchCounter { get; set; }
-
-        [JsonPropertyName("id")]
-        public int Id { get; set; }
-
-        [JsonPropertyName("is_baby")]
-        public bool IsBaby { get; set; }
-
-        [JsonPropertyName("is_legendary")]
-        public bool IsLegendary { get; set; }
-
-        [JsonPropertyName("is_mythical")]
-        public bool IsMythical { get; set; }
-
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("names")]
-        public IEnumerable<Name> Names { get; set; }
-
-        [JsonPropertyName("order")]
-        public int Order { get; set; }
-
-        [JsonPropertyName("pal_park_encounters")]
-        public IEnumerable<PalParkEncounter> PalParkEncounters { get; set; }
-
-        [JsonPropertyName("pokedex_numbers")]
-        public IEnumerable<PokedexNumber> PokedexNumbers { get; set; }
-
-        [JsonPropertyName("shape")]
-        public Shape Shape { get; set; }
-
-        [JsonPropertyName("varieties")]
-        public IEnumerable<Variety> Varieties { get; set; }
+       
     }
 
-    public class Shape
-    {
-        [JsonPropertyName("name")]
-        public string Name { get; set; }
-
-        [JsonPropertyName("url")]
-        public string Url { get; set; }
-    }
-
-    public class Variety
-    {
-        [JsonPropertyName("is_default")]
-        public bool IsDefault { get; set; }
-
-        [JsonPropertyName("pokemon")]
-        public Pokemon Pokemon { get; set; }
-    }
+    
 
     public class Version
     {
